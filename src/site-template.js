6bf76f420bcb8add0b62b39c1e42a00125ac1b2d
Josh <josh.haddix598@gmail.com>
2022-07-30 13:00:17 -0500

    Basic html layout complete

--- src/site-template.js
+++ src/site-template.js
@@ -1,74 +1,70 @@
-function cardBuilder(employee){
-    console.log(employee);
-    if(employee.getRole() === 'Manager'){
-        return `
-        <section class="card col-3 mx-3">
-            <div class="card-header">
-                <h2>${employee.name}</h2>
-                <i></i><h2>${employee.getRole()}</h2>
-            </div>
-            
-            <div>
-                <div>
-                    ID: ${employee.id}
+function cardBuilder(manager, engineers, interns){
+    let output = `
+            <section class="card col-3 mx-3 mb-3">
+                <div class="card-header bg-primary ">
+                    <h2>${manager.name}</h2>
+                    <h2><i class="fa-solid fa-clipboard-list"></i> ${manager.getRole()}</h2>
                 </div>
+                
                 <div>
-                    Email: <a href="mailto: ${employee.email}">${employee.email}</a>
+                    <div>
+                        ID: ${manager.id}
+                    </div>
+                    <div>
+                        Email: <a href="mailto: ${manager.email}">${manager.email}</a>
+                    </div>
+                    <div>
+                        Office Number: ${manager.officeNumber}
+                    </div>
                 </div>
+            </section>
+        `;
+    
+    for(engineer of engineers) {
+        output+= `
+            <section class="card col-3 mx-3 mb-3">
+                <div class="card-header bg-primary ">
+                    <h2>${engineer.name}</h2>
+                    <h2><i class="fa-solid fa-glasses"></i> ${engineer.getRole()}</h2>
+                </div>
+                
                 <div>
-                    Office Number: ${employee.officeNumber}
+                    <div>
+                        ID: ${engineer.id}
+                    </div>
+                    <div>
+                        Email: <a href="mailto: ${engineer.email}">${engineer.email}</a>
+                    </div>
+                    <div>
+                        GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         `
     }
-    else if(employee.getRole() === 'Engineer'){
-        return `
-        <section class="card col-3 mx-3">
-            <div class="card-header">
-                <h2>${employee.name}</h2>
-                <i></i><h2>${employee.getRole()}</h2>
-            </div>
-            
-            <div>
-                <div>
-                    ID: ${employee.id}
+    for(intern of interns) {
+        output+= `
+            <section class="card col-3 mx-3 mb-3">
+                <div class="card-header bg-primary ">
+                    <h2>${intern.name}</h2>
+                    <h2><i class="fa-solid fa-user-graduate"></i> ${intern.getRole()}</h2>
                 </div>
+                
                 <div>
-                    Email: <a href="mailto: ${employee.email}">${employee.email}</a>
+                    <div>
+                        ID: ${intern.id}
+                    </div>
+                    <div>
+                        Email: <a href="mailto: ${intern.email}">${intern.email}</a>
+                    </div>
+                    <div>
+                        School: ${intern.school}
+                    </div>
                 </div>
-                <div>
-                    GitHub: <a href="github.com/${employee.github}">${employee.github}</a>
-                </div>
-            </div>
-        </section>
+            </section>
         `
     }
-    else if(employee.getRole() === 'Intern') {
-        return `
-        <section class="card col-3 mx-3">
-            <div class="card-header">
-                <h2>${employee.name}</h2>
-                <i></i><h2>${employee.getRole()}</h2>
-            </div>
-            
-            <div>
-                <div>
-                    ID: ${employee.id}
-                </div>
-                <div>
-                    Email: <a href="mailto: ${employee.email}">${employee.email}</a>
-                </div>
-                <div>
-                    School: ${employee.school}
-                </div>
-            </div>
-        </section>
-        `
-    }
-    else {
-        return "Something went wrong"
-    }
+    return output;
 }
 
 function generateSite(managers,engineers,interns){
@@ -80,20 +76,17 @@ function generateSite(managers,engineers,interns){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <script src="https://kit.fontawesome.com/487bb64b3a.js" crossorigin="anonymous"></script>
         <title>Document</title>
     </head>
     <body>
-        <header>
+        <header class="bg-danger text-center py-5 mb-2 text-light">
             <h1>My Team</h1>
         </header>
 
         <main class="row justify-content-around">
 
-            ${cardBuilder(managers)}
-
-            ${engineers.map(cardBuilder)}
-
-            ${interns.map(cardBuilder)}
+            ${cardBuilder(managers,engineers,interns)}
 
         </main>
 
@@ -102,4 +95,6 @@ function generateSite(managers,engineers,interns){
     `
 }
 
+
+// Graphic design is not my passion 
 module.exports = generateSite;
